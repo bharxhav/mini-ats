34ea6bed9d0928613cfa2d5c497979e3bd618f7d
Bhargav Kantheti <bhargavkantheti5@gmail.com>
2024-02-05 11:00:20 -0500

    Update keywords

--- app.py
+++ app.py
@@ -69,14 +69,18 @@ def main():
             resume_evaluator = ResumeEvaluator()
             resume_text = resume_evaluator.input_pdf_text(uploaded_file)
             input_prompt = f"""
-            Act like a skilled or very experienced ATS (Application Tracking System) with a deep understanding of tech field, software engineering, data science, data analyst and big data engineer. Your task is to evaluate the resume based on the given job description. You must consider the job market is very competitive and you should provide best assistance for improving the resumes. Assign the percentage Matching based on Jd and the missing keywords with high accuracy.
+            Act like a skilled or very experienced ATS (Application Tracking System) with a deep understanding of data science and all related fields, data analyst, machine learning engineering and big data engineer. Your task is to evaluate the resume based on the given job description. You must consider the job market is very competitive and you should provide best assistance for improving the resumes. Assign the percentage Matching based on Jd and the missing keywords, and related keywords with high accuracy.
             resume: {resume_text}
             description: {jd}
         
             I want the response in one single string having the structure \
-            {{"JD Match":"%", 
-            "Missing Keywords" : [],
-            "Profile Summary" : ""}}
+            {{\n
+                "JD Match":"%", \n 
+                "Missing Keywords" : [], \n
+                "Hidden Keywords" : [], \n
+                "Good To Have Keywords : [], \n
+                "Profile Summary" : "" \n
+                }}
             """
 
             response = resume_evaluator.get_gemini_response(input_prompt)
